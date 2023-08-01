9346e5d5404281718ab1cadbe14c4d47a831aa71
han_kyeul <phk9436phk@gmail.com>
2023-08-01 20:48:21 +0900

    state를 이용해 컴포넌트 렌더링

--- src/App.js
+++ src/App.js
@@ -3,28 +3,26 @@ import { Component } from "./core/core";
 export default class App extends Component {
   constructor() {
     super({
-      tagName: "h1", //payload로 상속된 변수 할당
+      tagName: "div", //payload로 상속된 변수 할당
       state: {
-        inputText: "",
+        fruits: [
+          { name: "Apple", price: 1000 },
+          { name: "Banana", price: 2000 },
+          { name: "Orange", price: 3000 },
+        ],
       },
-    }); //상속 시 무조건 작성
+    }); //상속 시 작성
   }
   render() {
     //상속된 render 메서드를 작성한다.
-    this.el.classList.add("search");
     this.el.innerHTML = /*html*/ `
-      <input type="text" />
-      <button type="button">Click!</button>
+      <h1>Fruites</h1>
+      <ul>
+        ${this.state.fruits
+          .filter((el) => el.price < 3000)
+          .map((el) => `<li>name: ${el.name}, price: ${el.price}</li>`)
+          .join("")}
+      </ul>
     `;
-
-    const inputEl = this.el.querySelector("input");
-    inputEl.addEventListener("input", () => {
-      this.state.inputText = inputEl.value;
-    });
-
-    const buttonEl = this.el.querySelector("button");
-    buttonEl.addEventListener("click", () => {
-      console.log(this.state.inputText);
-    });
   }
 }
