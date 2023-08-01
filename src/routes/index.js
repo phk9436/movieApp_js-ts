951934b8c7d3a6f124fe2930260b0642d21c8891
han_kyeul <phk9436phk@gmail.com>
2023-08-02 01:00:00 +0900

    home 라우터 세팅

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,4 @@
 import { createRouter } from "../core/core";
+import Home from "../pages/Home";
 
-export default createRouter([
-]);
+export default createRouter([{ path: "#/", component: Home }]);
