685519c7fb1b7ae82b85e1c5b9855fe8d7085920
han_kyeul <phk9436phk@gmail.com>
2023-08-01 20:36:57 +0900

    state 설정 및 이벤트 핸들링

--- src/App.js
+++ src/App.js
@@ -4,10 +4,27 @@ export default class App extends Component {
   constructor() {
     super({
       tagName: "h1", //payload로 상속된 변수 할당
+      state: {
+        inputText: "",
+      },
     }); //상속 시 무조건 작성
   }
   render() {
     //상속된 render 메서드를 작성한다.
-    this.el.textContent = "Hello, world!";
+    this.el.classList.add("search");
+    this.el.innerHTML = /*html*/ `
+      <input type="text" />
+      <button type="button">Click!</button>
+    `;
+
+    const inputEl = this.el.querySelector("input");
+    inputEl.addEventListener("input", () => {
+      this.state.inputText = inputEl.value;
+    });
+
+    const buttonEl = this.el.querySelector("button");
+    buttonEl.addEventListener("click", () => {
+      console.log(this.state.inputText);
+    });
   }
 }
--- src/core/core.js
+++ src/core/core.js
@@ -1,12 +1,13 @@
 // Component //
 export class Component {
   constructor(payload = {}) {
-    const { tagName = "div" } = payload;
+    const { tagName = "div", state = {} } = payload;
     this.el = document.createElement(tagName);
+    this.state = state;
     this.render(); //상속한 render함수를 바로 동작시킨다.
   }
-  
-  render() { //상속해서 사용할 부분
 
+  render() {
+    //상속해서 사용할 부분
   }
 }
