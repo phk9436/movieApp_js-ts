dc63d5cedb153812d5fab2ebfedf18067c613c04
han_kyeul <phk9436phk@gmail.com>
2023-08-03 22:36:32 +0900

    404 not found 페이지 작업 완료

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import { createRouter } from "../core/core";
 import Home from "../pages/Home";
 import MovieDetail from "../pages/MovieDetail";
+import NotFound from "../pages/NotFound";
 
 export default createRouter([
   { path: "#/", component: Home },
-  { path: "#/movie", component: MovieDetail}
+  { path: "#/movie", component: MovieDetail },
+  { path: ".*", component: NotFound }, //아무 글자나 1글자 이상
 ]);
