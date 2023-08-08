144ad25a6ffcb327f2dd5c3cbd322922a5924723
han_kyeul <phk9436phk@gmail.com>
2023-08-09 01:24:03 +0900

    Router 타이핑 완료

--- src/core/core.ts
+++ src/core/core.ts
@@ -1,8 +1,11 @@
 // Component //
-import { IComponentPayload } from "../types/coreTypes";
+import { IComponentPayload, IRoute, IQuery } from "../types/coreTypes";
 
 export class Component {
-  constructor(payload = {}) {
+  el
+  state
+  props
+  constructor(payload:IComponentPayload = {}) {
     const { tagName = "div", state = {}, props = {} } = payload;
     this.el = document.createElement(tagName);
     this.state = state;
@@ -16,29 +19,32 @@ export class Component {
 }
 
 // Router //
-function routeRender(routes) {
+function routeRender(routes: IRoute[]) {
   if (!location.hash) {
     history.replaceState(null, "", "/#/");
   }
-  const routerView = document.querySelector("router-view");
   // https://localhost:1234/#/about?name=heropy
   // #/about?name=heropy
+  const routerView = document.querySelector("router-view");
+  if(!routerView) {
+    return;
+  }
   const [hash, queryString = ""] = location.hash.split("?");
-  const query = queryString.split("&").reduce((acc, cur) => {
+  const query = queryString.split("&").reduce((acc: IQuery, cur) => {
     const [key, value] = cur.split("=");
     acc[key] = value;
     return acc;
-  }, {});
+  }, {} as IQuery);
   history.replaceState(query, ""); //세번째 인수가 비워져 있으면 히스토리의 state에 첫번째 인수가 저장
   const currentRoute = routes.find(
     (e) => new RegExp(`${e.path}/?$`).test(hash) //routes 중 hash값과 일치하는 컴포넌트 찾음
   );
   routerView.innerHTML = "";
-  routerView.append(new currentRoute.component().el); //렌더링
+  currentRoute && routerView.append(new currentRoute.component().el); //렌더링
   window.scrollTo(0, 0);
 }
 
-export function createRouter(routes) {
+export function createRouter(routes: IRoute[]) {
   return function () {
     window.addEventListener("popstate", () => {
       routeRender(routes);
--- src/types/coreTypes.ts
+++ src/types/coreTypes.ts
@@ -1,9 +1,20 @@
+import { Component } from "../core/core";
+
 export interface IComponentPayload {
   tagName?: string;
   props?: {
-    [key: string]: unknown
+    [key: string]: unknown;
   };
   state?: {
-    [key: string]: unknown
+    [key: string]: unknown;
   };
+}
+
+export interface IRoute {
+  path: string;
+  component: typeof Component;
+}
+
+export interface IQuery {
+  [key: string]: string;
 }
